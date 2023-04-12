fc0b2f0fb5c17197598078b6fdc0ea9a7169b050
JoseAlejandroDeAbreuFernandes <josealejandro.deabreu@gmail.com>
2023-04-12 20:43:20 +0200

    FOTO 6

---
+++ Github.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+git config user.name "JAle05"
+git config user.Email "josealejandro.deabreu@gmail.com"
+git config user.password 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
+# git add .
+# git commit -m "Foto3"
+git remote add origin "https://github.com/JAle05/Tarea_12-4-23.git"
+git branch -M Main
+git push -u origin Main
