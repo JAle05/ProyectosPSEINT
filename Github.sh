fa87046de952b2a610e37df9ee369b0fbec7f060
JAle05 <130554595+JAle05@users.noreply.github.com>
2023-04-12 20:54:25 +0200

    Update Github.sh

--- Github.sh
+++ Github.sh
@@ -2,7 +2,7 @@
 clear
 git config user.name "JAle05"
 git config user.Email "josealejandro.deabreu@gmail.com"
-git config user.password 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
+git config user.password 
 # git add .
 # git commit -m "Foto3"
 git remote add origin "https://github.com/JAle05/Tarea_12-4-23.git"
